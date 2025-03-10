57cdd17512f5bf8f25f99824d4fc33c3b104a0fc
Rafael Moreno <rafaelmorenosax@gmail.com>
2025-03-10 20:18:36 -0300

    testa relacionamento

--- projetoescola/src/main/java/com/example/projetoescola/ProjetoescolaApplication.java
+++ projetoescola/src/main/java/com/example/projetoescola/ProjetoescolaApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.projetoescola.models.CategoriaCurso;
 import com.example.projetoescola.models.Curso;
+import com.example.projetoescola.repositories.CategoriaCursoRepository;
 import com.example.projetoescola.repositories.CursoRepository;
 
 @SpringBootApplication
@@ -16,14 +18,23 @@ public class ProjetoescolaApplication {
 
 	@Bean
 	public CommandLineRunner init(
-			@Autowired CursoRepository cursoRepository) {
+			@Autowired CursoRepository cursoRepository,
+			@Autowired CategoriaCursoRepository categoriaRepository) {
 		return args -> {
 			cursoRepository.salvar(
 					new Curso(null, "teste", 2000));
 			cursoRepository.salvar(
 					new Curso(null, "teste2", 2050));
 			List<Curso> listaCursos = cursoRepository.obterTodos();
+			System.out.println("*** Cursos cadastrados:");
 			listaCursos.forEach(System.out::println);
+			System.out.println("*** Inserir Categoria:");
+			CategoriaCurso categ = categoriaRepository.salvar(
+					new CategoriaCurso(null, "Informática"));
+			System.out.println("*** Vincular Categoria ao curso ***");
+			Curso curso = cursoRepository.obterTodos().get(0);
+			curso.setCategoriaCurso(categ);
+			cursoRepository.salvar(curso);
 		};
 	}
 
--- projetoescola/src/main/java/com/example/projetoescola/models/CategoriaCurso.java
+++ projetoescola/src/main/java/com/example/projetoescola/models/CategoriaCurso.java
@@ -2,9 +2,16 @@ package com.example.projetoescola.models;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
+@Entity
 public class CategoriaCurso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     @OneToMany(mappedBy = "categoriaCurso")
